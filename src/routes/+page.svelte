a5a038d28df4c42cefdcad05338927fa74794c30
Richard Vogt Jr <richardvogtjr@Richards-Mac-mini.local>
2023-01-13 19:34:20 -0700

    add alert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+    function sendMessage() {
+        alert("äliens have been contacted");
+    }
+</script>
+
 <h1>Welcome to Chatting With Aliens</h1>
 
-<input id="chat-a" type="text"><label for="chat-a"></label>
+<input id="chat-a" type="text"><button on:click={sendMessage}></button>
